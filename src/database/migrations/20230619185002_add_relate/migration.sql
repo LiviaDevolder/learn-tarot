64561616071f2cf509d972ceefc75568d9f731f2
Livia Devolder <livia.pinheiro-extern@renault.com>
2023-06-19 15:50:21 -0300

    feat: add relate table

---
+++ src/database/migrations/20230619185002_add_relate/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Relate" (
+    "id" TEXT NOT NULL,
+    "depoiment" TEXT NOT NULL,
+    "person" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "Relate_pkey" PRIMARY KEY ("id")
+);
